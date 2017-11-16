0ad99996bc8099f7ecf5f364b4c74257acb05b9a
Ye Wang <lhywang@wireless-10-145-92-153.public.utexas.edu>
2017-11-16 10:37:43 -0600

    modified training script

--- flower_baseline_train.py
+++ flower_baseline_train.py
@@ -57,4 +57,37 @@ def train_next_batch(SizeBatch):
 X = tf.placeholder(tf.float32, [SizeBatch, 227, 227, 3])
 Y_ = tf.placeholder(tf.float32, [SizeBatch, 10])
 
-
+#definition of weights in 1st conv layer
+WConv1 = tf.Variable(tf.truncated_normal([11, 11, 3, 64], 
+                                         stddev = 0.1))
+BConv1 = tf.Variable(tf.zeros([64]))
+#1st conv layer
+LayerConv1 = tf.nn.relu(tf.nn.conv2d(X, WConv1, strides=[1, 4, 4, 1], padding = 'SAME') + BConv1)
+#1st pool layer
+LayerPool1 = tf.nn.max_pool(LayerConv1, 
+                            ksize=[1, 3, 3, 1], 
+			    strides=[1, 2, 2, 1], 
+			    padding='VALID')
+#1st lrn
+LayerLRN1 = tf.nn.local_response_normalization(LayerPool1,
+                                               alpha=1e-4,
+                                               beta=0.75,
+                                               depth_radius=2, 
+					       bias=2.0)
+#definition of weights in 2nd conv layer
+WConv2 = tf.Variable(tf.truncated_normal([5, 5, 64, 192], 
+                                         stddev = 0.1))
+BConv2 = tf.Variable(tf.zeros([192]))
+#2nd conv layer
+LayerConv2 = tf.nn.relu(tf.nn.conv2d(LayerLRN1, WConv2, strides=[1, 1, 1, 1], padding = 'SAME') + BConv2)
+#2nd pool layer
+LayerPool2 = tf.nn.max_pool(LayerConv2,
+                            ksize=[1, 3, 3, 1],
+                            strides=[1, 2, 2, 1],
+                            padding='VALID')
+#2nd lrn layer
+LayerLRN2 = tf.nn.local_response_normalization(LayerPool2,
+                                               alpha=1e-4,
+                                               beta=0.75,
+                                               depth_radius=2,
+                                               bias=2.0)
